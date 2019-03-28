a89daa4beb150a92307423ea56410f8287599cfa
L42Project <laurent42@yahoo.com>
2019-03-28 10:00:00 +0100

    Mise a jour

--- Divers/tutoriel11/xmltoarray.py
+++ Divers/tutoriel11/xmltoarray.py
@@ -27,5 +27,12 @@ def xml_to_dataset(dir, size=None):
     for e in tab_label:
         tab_one_hot.append(np.eye(len(l))[l.index(e)])
         
-    return tab_image, tab_label, tab_one_hot
+    return tab_image, l, tab_one_hot
+
+tab_image, tab_label, tab_one_hot=xml_to_dataset("./", (32, 32))
+
+for i in range(len(tab_image)):
+    cv2.imshow('image', tab_image[i])
+    print(tab_label[np.argmax(tab_one_hot[i])], tab_one_hot[i])
+    cv2.waitKey()
 
