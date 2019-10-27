dd4b4b601c245f89370b1627bae1ab64b844cb57
L42Project <laurent42@yahoo.com>
2019-10-27 12:25:23 +0100

    Mise a jour

--- Divers/descente_gradient/comparaison.py
+++ Divers/descente_gradient/comparaison.py
@@ -21,7 +21,6 @@ Y=np.arange(-3, 3, 0.2)
 X, Y=np.meshgrid(X, Y)
 Z=fonction(X, Y)
 ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1)
-#ax.contour(X, Y, Z, 50, rstride=1, cstride=1, cmap='binary')
 plt.xlabel("Paramètre 1 (x)")
 plt.ylabel("Paramètre 2 (y)")
 
@@ -45,8 +44,8 @@ while True:
     x2=x2-lr*g_x2
     y2=y2-lr*g_y2
 
-    ax.scatter(x1, y1, fonction(x1, y1), marker='o', s=10, color='#FF0000', label="toto")
-    ax.scatter(x2, y2, fonction(x2, y2), marker='o', s=10, color='#00FF00', label="tutu")
+    ax.scatter(x1, y1, fonction(x1, y1), marker='o', s=10, color='#FF0000')
+    ax.scatter(x2, y2, fonction(x2, y2), marker='o', s=10, color='#00FF00')
     plt.draw()
     print("iteration= {} x1={:+7.5f} y1={:+7.5f} x2={:+7.5f} y2={:+7.5f}".format(i, x1, y1, x2, y2))
     plt.pause(0.05)
